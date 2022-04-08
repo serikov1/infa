d82cba48c34731ddd1eb1fdf6e6a9f36dde5695e
serikov1 <90526810+serikov1@users.noreply.github.com>
2022-04-08 11:41:43 +0300

    Update Hello_sky_in_the_clouds....cpp

--- homework/H_W 8 week/Hello_sky_in_the_clouds....cpp
+++ homework/H_W 8 week/Hello_sky_in_the_clouds....cpp
@@ -53,7 +53,7 @@ public:
      * Anny 5
      */
     void print_standings() const{
-        std::map<unsigned int, std::string> reverse_results;
+        std::multimap<unsigned int, std::string> reverse_results;
         for(auto&  st: results) reverse_results[st.second] = st.first;
         for(auto i = std::rbegin(reverse_results); i != std::rend(reverse_results); i++){
             std::cout<< i->second <<' '<<i->first<<std::endl;
